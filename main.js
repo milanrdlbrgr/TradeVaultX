4bf5b6468e70418d4743ed3a1e6cb8f07427560a
solsuccess <maxi.redelberger@gmail.com>
2026-03-18 11:04:16 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow, shell } = require('electron');
+const path = require('path');
+const fs = require('fs');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 900,
+    minHeight: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+    icon: path.join(__dirname, 'icon.png'),
+    titleBarStyle: 'default',
+    title: 'TradeVaultX',
+    backgroundColor: '#080808',
+    show: false,
+  });
+
+  mainWindow.loadFile('TradeVault-v3.html');
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+
+  // Remove default menu
+  mainWindow.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
